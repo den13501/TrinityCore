c3d564650b9f95574ba8d2eeea8dcd4d54f89961
Rochet2 <tqbattlenet@gmail.com>
2020-02-12 18:19:43 +0200

    Update FindBoost.cmake

--- cmake/macros/FindBoost.cmake
+++ cmake/macros/FindBoost.cmake
@@ -209,7 +209,7 @@ if (NOT Boost_NO_BOOST_CMAKE)
   # If we found boost-cmake, then we're done.  Print out what we found.
   # Otherwise let the rest of the module try to find it.
   if (Boost_FOUND)
-    message("Boost ${Boost_FIND_VERSION} found.")
+    message("Boost ${Boost_FIND_VERSION} asd found.")
     if (Boost_FIND_COMPONENTS)
       message("Found Boost components:")
       message("   ${Boost_FIND_COMPONENTS}")
